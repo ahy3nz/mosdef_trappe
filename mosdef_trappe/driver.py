605758d9232015cc962f4731695aeeca5078b2ee
Alex Yang <ahy3nz@virginia.edu>
2019-11-21 15:43:55 -0600

    Move engine simulate functions

--- mosdef_trappe/driver.py
+++ mosdef_trappe/driver.py
@@ -7,18 +7,9 @@ def main():
     cmpd = Propane()
     structure = build(cmpd, n_compounds=500, density=615.5*u.kg/u.m**3)
 
-    import mosdef_trappe.gromacs_util.gromacs_functions as gmx_funcs
-    gmx_funcs.simulate(structure, temperature=300*u.Kelvin, n_steps=5000,
-            pressure=None)
-    import mosdef_trappe.openmm_util.openmm_functions as omm_funcs
-    omm_funcs.simulate(structure, temperature=300*u.Kelvin, n_steps=5000,
-            pressure=None)
-    import mosdef_trappe.hoomd_util.hoomd_functions as hoomd_funcs
-    hoomd_funcs.simulate(structure, temperature=300*u.Kelvin, n_steps=5000,
-            pressure=None)
-    import mosdef_trappe.gomc_util.gomc_functions as gomc_funcs
-    gomc_funcs.simulate(structure, temperature=300*u.Kelvin)
-
+    simulate(structure, 'gromacs', 
+            temperature=300*u.Kelvin, pressure=None,
+            n_steps=5000)
 
     #import panedr
     #df = panedr.edr_to_df('md.edr')
@@ -70,6 +61,22 @@ def build(cmpd, density=0.5*u.gram/(u.cm**3), n_compounds=1000):
 
     return parametrized_structure
 
+def simulate(structure, engine, **kwargs):
+    if engine.lower() == 'gromacs':
+        import mosdef_trappe.gromacs_util.gromacs_functions as gmx_funcs
+        gmx_funcs.simulate(structure, **kwargs) 
+    elif engine.lower() == 'openmm':
+        import mosdef_trappe.openmm_util.openmm_functions as omm_funcs
+        omm_funcs.simulate(structure, **kwargs) 
+    elif engine.lower() == 'hoomd':
+        import mosdef_trappe.hoomd_util.hoomd_functions as hoomd_funcs
+        hoomd_funcs.simulate(structure, **kwargs) 
+    elif engine.lower() == 'gomc':
+        import mosdef_trappe.gomc_util.gomc_functions as gomc_funcs
+        gomc_funcs.simulate(structure, **kwargs)
+
+
 if __name__ == "__main__":
     main()
+
 
